2d23d92e2e11564337eca83509e21a1eee9c237d
Gustavo <rodrigues.gustavo2@aluno.ifsp.edu.br>
2024-08-13 00:36:29 -0300

    Auth, Ajuda,Handlers,um upload que eu achei e uma porta pro app

--- backend/app.js
+++ backend/app.js
@@ -37,5 +37,11 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 
 module.exports = app;
