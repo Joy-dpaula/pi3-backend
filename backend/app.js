5fbe0afa10e15d1d0ff8f70788fe68df537ffb77
Joy-dpaula <joycedpaula136@gmail.com>
2024-08-15 16:20:39 -0300

    app.js configurado

--- backend/app.js
+++ backend/app.js
@@ -1,50 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors'); 
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser';
+import logger from 'morgan'
+import cors from 'cors'
 
-var indexRouter = require('./src/routers/index');
+import accountRoutes from './src/routers/accountRoutes.js'
+// var indexRouter = require('./src/routers/index');
+// var usersRouter = require('./src/routers/accountRoutes');
+// var usersRouter = require('./src/routers/users');
+// var usuariosRoutes = require('./src/routers/accountRoutes');
 
-var usersRouter = require('./src/routers/accountRoutes');
-  
+const app = express(); 
 
-var usersRouter = require('./src/routers/users');
-var usuariosRoutes = require('./src/routers/accountRoutes');
-
-var app = express(); 
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors()); // Habilita CORS para todas as rotas
+app.use(cors()); 
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/usuarios', usuariosRoutes);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/usuarios', accountRoutes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+export default router
