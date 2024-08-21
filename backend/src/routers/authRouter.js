5e0ad16618c1370c68fc08b49a36cf8c83fba4f6
Betryz <betryz.f@aluno.ifsp.edu.br>
2024-08-21 16:44:22 -0300

    aaa

--- backend/src/routers/authRouter.js
+++ backend/src/routers/authRouter.js
@@ -1,10 +1,9 @@
 import express from 'express'
 
-import loginController from '../controllers/auth/loginController.js';
+
 
 const router = express.Router();
 
-router.post('/login',  loginController);
 
 
 export default router
