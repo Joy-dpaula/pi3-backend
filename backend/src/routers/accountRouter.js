d1f1966c02b44c8c24d3042c5be2fc7b13f4a74f
GustavoRodriguesk <161630558+GustavoRodriguesk@users.noreply.github.com>
2024-08-29 14:57:39 -0300

    mudei o login pq tava ruim
    dd

--- backend/src/routers/accountRouter.js
+++ backend/src/routers/accountRouter.js
@@ -4,6 +4,7 @@ import deleteAccount from '../controllers/account/deleteController.js';
 import updateAccount from '../controllers/account/updateController.js';
 import getAccountById from '../controllers/account/getByIdController.js';
 import getAccount from '../controllers/account/getController.js';
+import loginController from '../controllers/auth/loginController.js';
 
 import {authenticateToken}  from '../utils/auth.js'; // Certifique-se de que o caminho está correto
 
@@ -22,6 +23,7 @@ router.get('/', getAccount);
 
 // Rota pública para obter uma conta específica por ID
 router.get('/:id', getAccountById);
+router.post('/login', loginController);
 
 // Aplicar autenticação para as seguintes rotas
 router.patch('/:id', authenticateToken, updateAccount);
