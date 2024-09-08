924a5b8a4dd0734781d63d60d8f345141e1ff744
Betryz <beatryz.f@aluno.ifsp.edu.br>
2024-09-08 19:08:25 -0300

    de novo

--- backend/src/utils/cookie.js
+++ backend/src/utils/cookie.js
@@ -5,7 +5,7 @@ export function configureCookieParser(app, secret) {
   app.use(cookieParser(secret));
 }
 
-// Função para definir um cookie criptografado
+
 export function setCookie(res, name, value, options = {}) {
   const isProduction = process.env.NODE_ENV === 'production';
 
@@ -13,17 +13,14 @@ export function setCookie(res, name, value, options = {}) {
     maxAge: 24 * 60 * 60 * 1000, // 24 horas
     httpOnly: true, // Segurança
     secure: isProduction, // HTTPS apenas em produção
-    sameSite: isProduction ? 'None' : 'Lax', // 'None' para produção, 'Lax' para dev
     ...options // Permite sobrescrever opções
   };
 
-  // Verificação para evitar problemas com 'None' sem 'secure: true'
-  if (cookieOptions.sameSite === 'None' && !cookieOptions.secure) {
-    throw new Error("sameSite 'None' requires 'secure: true' in production.");
-  }
-
+  // Remova temporariamente sameSite para depuração
   res.cookie(name, value, cookieOptions);
 }
+
+
 
 
 
