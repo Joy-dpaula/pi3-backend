af5aae7e8c00e3ee279cb4142aaf4f64fc974d55
Betryz <betryz.f@aluno.ifsp.edu.br>
2024-09-09 16:44:50 -0300

    models cadastro pronto

--- backend/src/routers/accountRouter.js
+++ backend/src/routers/accountRouter.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 // Importa os controladores para a criação, leitura, atualização e exclusão de contas
-import createAccount from '../controllers/account/createController.js';
+import {createAccount} from '../controllers/account/createController.js';
 import deleteAccount from '../controllers/account/deleteController.js';
 import updateAccount from '../controllers/account/updateController.js';
 import getAccountById from '../controllers/account/getByIdController.js';
