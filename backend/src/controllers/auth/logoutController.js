223df7bad4a38854c67ea182a5ccc401fe40c384
Betryz <beatryzfreitas6@gmail.com>
2024-09-24 14:08:55 -0300

    base para fazer o codigo do logout

--- backend/src/controllers/auth/logoutController.js
+++ backend/src/controllers/auth/logoutController.js
@@ -1,4 +1,19 @@
-import { PrismaClient } from '@prisma/client'
+import  { createSession, deleteSession, checkSession } from '../../models/authModel.js'
 
-const prisma = new PrismaClient();
+export default async function logout(req, res) {
+    const { email, token } = req.body
+  
+    sessionModel.deleteSession(email, token, (error, result) => {
+      if (error)
+        res.status(500).json({ message: "Erro no Banco de Dados" })
+      if (result) {
+        if (result.affectedRows) {
+          res.json({ message: "Usuário deslogado com sucesso!" })
+        } else {
+          res.status(404).json({ message: `Session não encontrada` })
+        }
+      }
+    })
+  }
+  
 
--- backend/src/models/authModel.js
+++ backend/src/models/authModel.js
@@ -51,3 +51,51 @@ export const loginModel = async (email, senha, res) => {
 
     return { usuario, accessToken };
 }
+
+
+export const createSession = (userId, token, callback) => {
+    const sql = 'INSERT INTO sessions (id_user, session) VALUES (?, ?);'
+    const values = [userId, token]
+    con.query(sql, values, (err, result) => {
+      if (err) {
+        callback(err, null)
+        console.log(`DB Error: ${err.sqlMessage}`)
+      } else {
+        callback(null, result)
+      }
+    })
+  }
+  
+
+  export const checkSession = (token, callback) => {
+    const sql = `
+      SELECT s.id_user, u.roles
+      FROM sessions as s
+      INNER JOIN users as u
+      ON s.id_user = u.id
+      WHERE s.session = ?;
+      `
+    const values = [token]
+    con.query(sql, values, (err, result) => {
+      if (err) {
+        callback(err, null)
+        console.log(`DB Error: ${err.sqlMessage}`)
+      } else {
+        callback(null, result)
+      }
+    })
+  }
+
+  export const deleteSession = (email, token, callback) => {
+    const sql = 'DELETE FROM sessions WHERE id_user = (SELECT id FROM users WHERE email = ?) AND session = ?;'
+    const value = [email, token]
+    con.query(sql, value, (err, result) => {
+      if (err) {
+        callback(err, null)
+        console.log(`DB Error: ${err.sqlMessage}`)
+      } else {
+        callback(null, result)
+      }
+    })
+  }
+   
