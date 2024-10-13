750f47eb2b042c2b12a4b954581d8fc4b6f7049f
Betryz <beatryz.f@aluno.ifsp.edu.br>
2024-10-13 00:26:04 -0300

    foi

--- backend/src/controllers/account/deleteController.js
+++ backend/src/controllers/account/deleteController.js
@@ -5,11 +5,21 @@ export const deleteAccount = async (req, res) => {
     const { id } = req.params; 
     const userToken = req.user; 
 
+    console.log("Informações do usuário autenticado:", userToken);
 
-    if (!userToken || !userToken.email) {
-        return res.sendStatus(401);
+    if (!userToken || typeof userToken.isAdmin === 'undefined' || typeof userToken.id === 'undefined') {
+        return res.status(401).json({ error: "Usuário não autenticado ou token inválido." });
     }
 
+    const isAdmin = userToken.isAdmin;
+    const userId = userToken.id;
+
+    console.log('isAdmin:', isAdmin);  
+    console.log('ID do usuário:', userId); 
+
+    if (!isAdmin && String(id) !== String(userId)) {
+        return res.status(403).json({ error: "Você não tem permissão para deletar este usuário." });
+    }
     try {
         const usuario = await getUsuarioById(id);
 
