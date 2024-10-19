72cb4bb44033cd1407517fe69f5e706e3ae8a954
Gustavo <rodrigues.gustavo2@aluno.ifsp.edu.br>
2024-10-19 13:59:49 -0300

    dd

--- backend/src/models/accountModel.js
+++ backend/src/models/accountModel.js
@@ -1,38 +1,55 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from 'bcryptjs';
-import { z } from 'zod';
 import { DateTime } from 'luxon';
+import { z } from 'zod';
 
 const prisma = new PrismaClient();
-const gmt3Date = DateTime.now().setZone('America/Sao_Paulo');
 
-const userSchema = z.object({
-    nome: z.string().min(1, { message: "Nome deve ser obrigatório!" }),
-    cpf: z.string().length(11, { message: "CPF deve ter 11 dígitos!" }),
-    email: z.string().email({ message: "Email inválido!" }).max(200),
-    senha: z.string().min(8, { message: "A senha deve ter no mínimo 8 caracteres!" }),
-    telefone: z.string().min(10, { message: "Telefone deve ter no mínimo 10 dígitos!" }).max(15),
-    nascimento: z.date().optional(), // Adicionando validação para a data de nascimento
-    cidade: z.string().optional(),
-    estado: z.string().optional(),
-    isAdmin: z.boolean().optional(),
-    foto_perfil: z.string().optional() // Esperamos que seja uma URL da imagem
-});
+const gmt3Date = DateTime.now().setZone('America/Sao_Paulo');
 
 console.log("Current time in GMT-3:", gmt3Date.toString());
 
-export async function createNewUser({ nome, email, senha, cpf, telefone, nascimento, isAdmin, cidade, estado, foto_perfil }) {
-    // Validar o usuário antes de criar
-    const existingUsuario = await prisma.usuario.findUnique({ where: { email } });
+const passwordSchema = z.string()
+    .min(8, { message: "A senha deve ter um tamanho mínimo de 8 caracteres." })
+    .regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[\W_]).{8,}$/, {
+        message: "Senha deve ter pelo menos 8 caracteres (incluindo letras maiúsculas, minúsculas, números e caracteres especiais)"
+    });
 
-    if (existingUsuario) {
-        return null; 
+const userSchema = z.object({
+    nome: z.string().min(1, { message: "Nome deve ser obrigatório" }),
+    cpf: z.string().length(11, { message: "CPF deve conter 11 dígitos" }),
+    email: z.string().email({ message: "Email em formato inválido" }).max(200),
+    senha: passwordSchema,
+});
+
+export async function createNewUser({ nome, email, senha, cpf, telefone, nascimento, isAdmin, cidade, estado, foto_perfil }) {
+
+    const result = userSchema.safeParse({ nome, cpf, email, senha })
+
+    if (!result.success) {
+        const errors = result.error.errors.map(err => err.message).join(", ");
+        throw new Error(errors);
+    }
+
+    const existingCpf = await prisma.usuario.findUnique({
+        where: { cpf }
+    });
+
+    const existingEmail = await prisma.usuario.findUnique({
+        where: { email }
+    });
+
+    if (existingCpf) {
+        throw new Error("Esse CPF já está em uso por outro usuário.");
+    }
+
+    if (existingEmail) {
+        throw new Error("Esse Email já está em uso por outro usuário.");
     }
 
     const hashedSenha = await bcrypt.hash(senha, 12);
-    const dataRegistroUTC = gmt3Date.toUTC().toJSDate();
+    const dataRegistroUTC = DateTime.now().setZone('America/Sao_Paulo').toUTC().toJSDate();
 
-    // Criar o novo usuário
     const usuario = await prisma.usuario.create({
         data: {
             nome,
@@ -40,28 +57,24 @@ export async function createNewUser({ nome, email, senha, cpf, telefone, nascime
             senha: hashedSenha,
             cpf: cpf.toString(),
             telefone: telefone.toString(),
-            nascimento: nascimento ? new Date(nascimento) : null,
+            nascimento:  nascimento ? new Date(nascimento) : null,
             isAdmin: isAdmin || false,
             cidade,
             estado,
-            foto_perfil, // Usando a variável foto_perfil corretamente
-            data_registro: dataRegistroUTC 
+            foto_perfil,
+            data_registro: dataRegistroUTC,
         },
         select: {
             id: true,
             nome: true,
             email: true,
             isAdmin: true,
-            data_registro: true,
-            cidade: true,
-            estado: true,
-            foto_perfil: true
+            nascimento: true,
         }
     });
 
     return usuario;
 }
-
 
 export async function getUsuarios() {
     const usuarios = await prisma.usuario.findMany();
@@ -70,23 +83,67 @@ export async function getUsuarios() {
 
 export async function getUsuarioById(id) {
     const account = await prisma.usuario.findUnique({
-        where: { id: Number(id) },
+        where: { id: String(id) },
     });
     return account;
 }
 
 export const deleteUsuarioById = async (id) => {
     return await prisma.usuario.delete({
-        where: { id: Number(id) },
+        where: { id: String(id) },
     });
 };
 
-export const update = async (usuario) => {
-    const result = await prisma.usuario.update({
-        data: usuario,
-        where:{
-           id: usuario.id 
-        }
-    })
-    return result
-}
+export const update = async (id, data) => {
+    if (!id) {
+        throw new Error('ID não fornecido');
+    }
+
+   
+
+    try {
+        const updatedUsuario = await prisma.usuario.update({
+            where: { id: id },
+            data,
+            select: {
+                id: true,
+                nome: true,
+                email: true,
+                isAdmin: true,
+            },
+        });
+
+        return updatedUsuario;
+    } catch (error) {
+        console.error('Update failed:', error);
+        throw new Error('Failed to update user');
+    }
+};
+
+export const getAccountById = async (id) => {
+    const account = await prisma.usuario.findUnique({
+        where: { id: id },
+    });
+    return account;
+};
+
+export const getAccounts = async () => {
+    try {
+        const accounts = await prisma.usuario.findMany({
+            select: {
+                id: true,
+                nome: true,
+                email: true,
+                cpf: true,
+                telefone: true,
+                nascimento: true,
+                data_registro: true,
+                isAdmin: true
+            }
+        });
+        return accounts;
+    } catch (error) {
+        console.error('Erro ao buscar contas:', error);
+        throw new Error('Não foi possível buscar as contas');
+    }
+};
